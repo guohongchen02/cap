ba02be70ecd6ca0c793872b1d5c5a454d07b4ec8
Guohong <guohong.chen@pcc.edu>
2017-06-20 00:39:32 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php require_once ('includes/config.php'); ?>
+
+<?php require_once ('includes/header_meta.php'); ?>
+	
+<title>About | <?php echo $webname; ?></title>	
+	
+<meta name="keywords" content="<?php echo $aboutkeywords; ?>">
+<meta name="description" content="<?php echo $aboutdescription; ?>">
+
+<?php require_once ('includes/header.php'); ?>
+
+<?php require_once ('includes/slidershow.php'); ?>	
+	
+<?php require_once ('includes/nav.php'); ?>
+	
+<section>
+	
+	<article class="content2">
+		
+		<h2>The Restaurant!</h2>
+		
+		
+		<br>
+		<p>The Executive Chef of the Ocean City Seafood Restaurant – Mr. Feng and his team were trained and gained their experience in culinary arts in many years. Recognized for their culinary skills by local restaurant critics, they prepare the delectable menus using only the freshest and highest quality ingredients.</p>		
+		<p> </p>
+		
+    </article>
+   
+</section>
+	
+<?php require_once ('includes/footer.php'); ?>
